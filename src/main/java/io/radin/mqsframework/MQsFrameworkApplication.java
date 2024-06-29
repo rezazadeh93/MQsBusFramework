d14b3b6e491804fd903d9fcc3cf157e593b908d9
amin <amin.rezazadeh93@gmail.com>
2024-06-29 21:15:44 +0330

    init project

---
+++ src/main/java/io/radin/mqsframework/MQsFrameworkApplication.java
@@ -0,0 +1,13 @@
+package io.radin.mqsframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQsFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQsFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/radin/mqsframework/MQsFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.radin.mqsframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MQsFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
